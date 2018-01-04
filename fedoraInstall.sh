9e29644d0e5dfaf9c557fe615b75f3bff3c37a4b
Sara Murray <magicalraccoon@users.noreply.github.com>
2018-01-03 19:09:40 -0600

    Update fedoraInstall.sh

--- fedoraInstall.sh
+++ fedoraInstall.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+sudo dnf update -y
+
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo rpm --import https://dl.itch.ovh/archive.key
 wget https://bintray.com/itchio/rpm/rpm -O itchio.repo
 sudo mv itchio.repo /etc/yum.repos.d/
 
-sudo dnf config-manager --add-repo https://repos.wine-staging.com/wine/fedora/26/winehq.repo
+### Fedora should have wine-staging by default, need to check ###
+#sudo dnf config-manager --add-repo https://repos.wine-staging.com/wine/fedora/26/winehq.repo
+
+
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
 sudo dnf copr enable konimex/neofetch -y
 
 sudo dnf install -y http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 
-sudo dnf install -y zsh dnf-plugins-core vim zsh tmux steam  weechat keepass spotify itch gnome-tweak-tool dwarffortress gnome-themes-standard gtk-murrine-engine neofetch autoconf automake pkg-config gtk3-devel flash-plugin vlc chromium gcc lsb-core-noarch chrome-gnome-shell htop wine VirtualBox tmux-powerline powerline
-
-sudo dnf update -y
+sudo dnf install -y zsh dnf-plugins-core vim zsh tmux steam  weechat keepass spotify itch gnome-tweak-tool dwarffortress gnome-themes-standard gtk-murrine-engine neofetch autoconf automake pkg-config gtk3-devel flash-plugin vlc chromium gcc lsb-core-noarch chrome-gnome-shell htop wine VirtualBox tmux-powerline powerline virtualbox
 
 wget https://github.com/atom/atom/releases/download/v1.23.1/atom.x86_64.rpm
 sudo rpm -i atom.x86_64.rpm
@@ -22,6 +25,8 @@ rm atom.x86_64.rpm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+### I keep switching back and forth between SolArc-Dark and Adwaita-Dark ###
 #sudo rm -rf /usr/share/themes/{SolArc,SolArc-Darker,SolArc-Dark}
 #rm -rf ~/.local/share/themes/{SolArc,SolArc-Darker,SolArc-Dark}
 #rm -rf ~/.themes/{SolArc,SolArc-Darker,SolArc-Dark}
@@ -32,7 +37,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 #rm -rf solarc-theme
 
 pip install --user powerline-status
-# pip3 install --user powerline-status
 
 git clone https://github.com/altercation/vim-colors-solarized.git
 cd vim-colors-solarized/colors
@@ -47,16 +51,19 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 echo "All done!"
 exit
 
-#fstab
+### fstab ###
+# 10.0.0.4:/media/streaming/Storage  /home/raccoon/Storage  nfs  defaults 0 0
+# 10.0.0.4:/media/streaming/Video  /home/raccoon/Videos  nfs  defaults 0 0
+# 10.0.0.4:/media/streaming/Music  /home/raccoon/Music  nfs  defaults 0 0
+
 
 # powerline-reload
 
-# disable wayland 'cause it's not ready yet!!
+### disable wayland 'cause it's not ready yet!! ###
 # sudo vim /etc/gdm/custom.conf 
 
 ### manual installs ###
 #hack font
-#virtualbox
 
 ### include ###
 #.bashrc
