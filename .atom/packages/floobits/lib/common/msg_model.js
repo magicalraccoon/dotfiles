975c216af8f5eec9f01251bc33d961e5acc46afa
Sara Murray <saramurray@candypunk.io>
2018-10-20 18:25:25 -0700

    RESTRUCTURE

--- .atom/packages/floobits/lib/common/msg_model.js
+++
@@ -1,8 +0,0 @@
-/* @flow weak */
-/** @jsx React.DOM */
-/** @fileOverview User preferences. */
-"use strict";
-
-const flux = require("flukes");
-
-module.exports = new flux.List();
--- .atom/packages/floobits/lib/common/permission_model.js
+++
@@ -1,6 +0,0 @@
-/* @flow weak */
-"use strict";
-
-var flux = require("flukes");
-
-module.exports = new flux.List([]);
